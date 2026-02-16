14711de0754654117f18774e4ed3792ed6c91838
yaelmtzk <jessianeroque@gmail.com>
2026-02-17 00:21:13 +0200

    Refactor production configuration to remove hardcoded MongoDB URL

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,5 @@
 export default {
-  dbURL: process.env.MONGO_URL ||
-    'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/?retryWrites=true&w=majority',
+  dbURL: process.env.MONGO_URL,
   dbName: process.env.DB_NAME || 'story_db'
 }
 
